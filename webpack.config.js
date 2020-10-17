c11a4887494641c30ef63f3ce39043681df7704d
JJonggTaeng <jjojjo141995@gmail.com>
2020-10-17 23:21:00 +0900

    20201017 webpack react

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack')
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: "./src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: { // 로더 설정하는 곳 바벨이랑 css-loader 설정하면될거같음
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg|gif0)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+        },
+      },
+
+    ]
+  },
+  // resolve: { extensions: ["*", ".js", ".jsx"] },
+  plugins: [ // HtmlWebpackPlugin, BannerPlugin, CleanWebpackPlugin, MiniCssExtractPlugin
+    new webpack.BannerPlugin({
+      banner: JSON.stringify(new Date().toLocaleString()),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin({})
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: '/',
+    overlay: true,
+    stats: "errors-only",
+    hot: true,
+  }
+}
